9f596004e8f8ec1918242359622418eade310c2a
ZosyaGlory <andrey.zosimov.95@gmail.com>
2020-12-06 17:01:47 +0300

    Remove unused import

--- src/main/java/net/proselytez/springbootdemoz/controller/BookController.java
+++ src/main/java/net/proselytez/springbootdemoz/controller/BookController.java
@@ -3,7 +3,6 @@ package net.proselytez.springbootdemoz.controller;
 import java.util.List;
 import net.proselytez.springbootdemoz.model.Book;
 import net.proselytez.springbootdemoz.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
