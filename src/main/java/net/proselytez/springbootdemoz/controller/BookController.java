5f341ad240fd0c0e5c4cff334464f1e1b1f228b6
Nesterov <al.nest.pav@gmail.com>
2020-12-10 01:17:49 +0300

    Add comments

--- src/main/java/net/proselytez/springbootdemoz/controller/BookController.java
+++ src/main/java/net/proselytez/springbootdemoz/controller/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController // используется вместо @Controller, когда мы не используем model (как в MVC), а обмениваемся объектами в формате JSON (по умолчанию)
 @RequestMapping("/books")
 public class BookController {
 
@@ -16,6 +16,17 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
+    /* RESTful - вместо url book-create, book-update, book-delete используем единый url, но разные методы HTTP
+    *  Create - POST-запрос
+    *  Read - GET-запрос
+    *  Update - PUT или PATCH (PATCH - если обновление только части объекта, а не всего),
+    *  Delete - DELETE-запрос
+    *  можно почитать
+    *  https://habr.com/ru/post/38730/
+    *  https://javarush.ru/groups/posts/2486-obzor-rest-chastjh-1-chto-takoe-rest - там 3 части
+    *  а для тестирования REST API удобно использовать Postman программу Тут можно почитать https://link.medium.com/i9I5TIos5bb
+     * */
 
     @PostMapping
     public ResponseEntity createBook(@RequestBody Book book){
@@ -36,7 +47,7 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateBook(@PathVariable("id") Long id, @RequestBody Book book){
+    public ResponseEntity updateBook(@PathVariable("id") Long id, @RequestBody Book book){ // тут мы id параметр не использовали, но по-хорошему, надо проверить id == book.getId(), если нет - то ошибка
         Book updatedBook = bookService.saveBook(book);
         return ResponseEntity.ok(updatedBook);
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,34 +1,22 @@
 
-getAll();
+getAllBooks(); // как страница загружается - сразу загружаем список всех книг
 
-var editBookId;
 
-function passData(bookId, bookName, bookAuthor) {
-    editBookId = bookId;
-
-    document.getElementById('editBookName').value = bookName;
-    document.getElementById('editBookAuthor').value = bookAuthor;
-}
-
-function getAll() {
+function getAllBooks() {
     let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', '/books', false);
+    xhr.open('GET', '/books', false); // создаем запрос типа GET по адресу /books
 
     try {
-        xhr.send();
-        if (xhr.status != 200) {
+        xhr.send(); // отправляем запрос
+        if (xhr.status !== 200) { // если статус ответа на запрос не 200 (OK) - значит какая-то ошибка (хотя статусов HTTP существует много и не всегда не ОК означает ошибку)
             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
-            var tableBody = document.getElementById('bookTableBody');
 
-            var books = JSON.parse(xhr.response);
+            var books = JSON.parse(xhr.response); // ответ у нас в формате json, нужно его конвертировать в объект JavaScript, для этого используем JSON.parse() функцию
 
-            console.log(books);
+            console.log(books); // выводим в консоле браузера список книг
 
-            //books.sort((a, b) => a.id < b.id);
-
-            books.sort(function(a, b) {
+            books.sort(function(a, b) { // отсортируем по возрастанию id
                 if (a.id > b.id) {
                     return 1; }
                 if (a.id < b.id) {
@@ -36,111 +24,118 @@ function getAll() {
                 return 0;
             });
 
-            tableBody.innerHTML = "";
+            var tableBody = document.getElementById('bookTableBody');
+            tableBody.innerHTML = ""; // найдем в html-шаблоне элемент с таким id, который представляет собой тело таблица, удалим все, что сейчас там находится
 
-            for (book of books) {
+            for (book of books) { // для каждой книге в списке книг добавляем в тело таблицы строку с значениями книги и с кнопками
                 tableBody.innerHTML += `
         
-            <tr>
-                <td>${book.id}</td>
-                <td>${book.name}</td>
-                <td>${book.author}</td>
-                <td><button type="button" class="btn btn-info" data-toggle="modal" data-target="#editBookModal" onclick="passData(${book.id}, '${book.name}', '${book.author}')">Edit</button></td>
-                <td><button type="button" class="btn btn-danger" onclick="deleteBook(${book.id})">Delete</button></td>
-            </tr>
-        
-        
-        `;
+                    <tr>
+                        <td>${book.id}</td>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>
+                            <button type="button"
+                                    class="btn btn-info"
+                                    data-toggle="modal"
+                                    data-target="#editBookModal"
+                                    onclick="setBookDataInEditModal(${book.id}, '${book.name}', '${book.author}')">Edit  
+                            </button>
+                             <!-- Тут какая-то магия Bootstrap: data-toggle и data-target, чтобы при нажатии на кнопку открывось модальное окно editBookModal
+                            Но нам нужно не просто открыть окно, но и передать туда данные, поэтому мы навесили на событие onclick функцию setBookDataInEditModal(),
+                            в которую передаем параметры книги -->     
+                        </td>
+                        <td><button type="button" class="btn btn-danger" onclick="deleteBook(${book.id})">Delete</button></td> <!-- При нажатии на кнопку Delete удаляем книги по id -->
+                    </tr>
+                    
+                `;
             }
-
-
         }
-    } catch (err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+    } catch (err) { // для отлова ошибок
         alert("Запрос не удался");
     }
 }
 
+// Устанавливаем в поля ввода в модальном окне редактирования значения текущей выбранной книги
+function setBookDataInEditModal(bookId, bookName, bookAuthor) {
+    document.getElementById('editBookId').value = bookId;
+    document.getElementById('editBookName').value = bookName;
+    document.getElementById('editBookAuthor').value = bookAuthor;
+}
 
-//data-toggle="modal" data-target="#editBookModal"
-
-
+// удаление книги по id при нажатии на Delete кнопку
 function deleteBook(bookId) {
 
     let xhr = new XMLHttpRequest();
-    xhr.open('DELETE', '/books/' + bookId, false);
-
+    xhr.open('DELETE', '/books/' + bookId, false); // запрос методом DELETE
 
     try {
         xhr.send();
-        if (xhr.status != 200) {
+        if (xhr.status !== 200) {
             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
-            //alert("Книга удалена");
-
-            getAll();
-
-
+            // книга успешно удалена, теперь обновим список, чтобы таблица содержала актуальные данные
+            getAllBooks();
         }
-    } catch (err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+    } catch (err) {
         alert("Запрос не удался");
     }
 
-
 }
 
-
+// создание книги при нажатии на Create кнопку в модальном окне
 function createBook() {
 
     var name = document.getElementById('createBookName').value;
-    var author = document.getElementById('createBookAuthor').value;
+    var author = document.getElementById('createBookAuthor').value; // получаем параметры книги из полей ввода
 
-    var book = {name: name, author: author};
+    var book = {name: name, author: author}; // создаем объект, описывающий книгу
 
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/books/', false);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.open('POST', '/books/', false); // запрос методом POST
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); // в RESTful мы используем json формат для передачи данных
 
 
     try {
-        xhr.send(JSON.stringify(book));
-        if (xhr.status != 200) {
+        xhr.send(JSON.stringify(book)); // переводим объект книга в строку json с помощью функции JSON.stringify и отправляем запрос
+        if (xhr.status !== 200) {
             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
-            getAll();
-
-            $('#createBookModal').modal('hide');
+            // книга успешно добавлена, обновляем список книг
+            getAllBooks();
+            $('#createBookModal').modal('hide'); // скрываем модальное окно
         }
-    } catch (err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+    } catch (err) {
         alert("Запрос не удался");
     }
 
 
 }
 
+// редактирование книги, при нажатии на Edit кнопку в модальном окне, аналогично функции createBook
 function editBook() {
 
     var name = document.getElementById('editBookName').value;
     var author = document.getElementById('editBookAuthor').value;
+    var id = document.getElementById('editBookId').value;
 
-    var book = {id: editBookId, name: name, author: author};
+    var book = {id: id, name: name, author: author};
 
     let xhr = new XMLHttpRequest();
-    xhr.open('PUT', '/books/' + editBookId, false);
+    xhr.open('PUT', '/books/' + id, false); // метод PUT обычно используется для редактировании ресурса в RESTful приложениях
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
 
     try {
         xhr.send(JSON.stringify(book));
-        if (xhr.status != 200) {
+        if (xhr.status !== 200) {
             alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
-            getAll();
-
+            getAllBooks();
             $('#editBookModal').modal('hide');
         }
     } catch (err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
         alert("Запрос не удался");
     }
-
 
 }
